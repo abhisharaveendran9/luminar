49491b7974692a437f31a55707c38946323f343a
abhisharaveendran <abhisharaveendran@gmail.com>
2022-06-06 11:33:56 +0530

    adding list program

---
+++ listworks/listdemo.py
@@ -0,0 +1,67 @@
+# name=["python","c++","c","java","javascript"]
+# print(name)
+#
+# for i in range(0,len(name)):
+#     print(name[i])
+#
+# exp=[10,33,18,87,54,77,100]
+# for i in range(0,5):
+#     print(exp[i])
+
+
+numbers=[10,12,15,16,17,18]
+
+# for num in numbers:
+#     if(num%2==0):
+#         print(num)
+
+# for num in numbers:
+#     if num>15:
+#         print(num+1)
+#     elif(num<15):
+#         print(num-1)
+#     elif(num==15):
+#         print(num)
+
+# count=0
+# for num in numbers:
+#     if num>=14 and num<=18:
+#         count=count+1
+# print(count)
+
+
+numbers=[2,1,-1,-5,4,-3,5,9,-6,-4,8,0,0]
+
+# p_count=0
+# n_count=0
+# for num in numbers:
+#     if num>0:
+#         p_count+=num
+#     if num<0:
+#         n_count+=num
+# print(p_count,n_count)
+
+
+# n_count=0
+# p_count=0
+# z_count=0
+
+#
+# for num in numbers:
+#     if num>0:
+#         p_count+=1
+#     elif num<0:
+#         n_count+=1
+#     elif num==0:
+#         z_count+=1
+# print(f"+ve count {p_count}, neg count {n_count},zero count {z_count}")
+
+
+num=[12,62,14,13,15,16,17]
+sum=0
+
+for n in num:
+    sum=sum+n
+print(sum)
+
+
